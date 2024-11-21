659df3916fef1545a70d257d0c3fa5798d82a902
Jonathan Borja <jborja.one@gmail.com>
2024-11-20 18:32:54 -0700

    done

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("hello", false)]
+        public void Test1(string word, bool expected)
         {
-
+            WordSmith result = new WordSmith();
+            bool actual = result.IsPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
